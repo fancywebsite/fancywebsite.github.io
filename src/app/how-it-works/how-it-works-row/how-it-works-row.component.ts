ca131bc173148538c296f6865a1a9b0716e0daeb
TimeaNagyGarwan <timea.nagy@garwan.sk>
2022-09-15 13:48:35 +0200

    updated text

--- src/app/how-it-works/how-it-works-row/how-it-works-row.component.ts
+++ src/app/how-it-works/how-it-works-row/how-it-works-row.component.ts
@@ -1,38 +1,14 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AnimationComponent } from 'src/app/animation.component';
 
 @Component({
   selector: 'app-how-it-works-row',
   templateUrl: './how-it-works-row.component.html'
 })
-export class HowItWorksRowComponent {
-  animation: boolean = false;
-  @Input() delay = '';
+export class HowItWorksRowComponent extends AnimationComponent {
   @Input() imgSrc = '';
   @Input() subtitle = '';
   @Input() description = '';
   @Input() rowType = 'left' || 'right';
   @Input() rowClass = '';
-
-  @HostListener('window:scroll', ['$event'])
-  isScrolledIntoView() {
-
-      const el = document.getElementById(this.delay);
-      if (el) {
-        const rect = el.getBoundingClientRect();
-        const topShown = rect.top >= 0;
-        const bottomShown = rect.bottom <= window.innerHeight;
-
-        const temp = topShown && bottomShown;
-        if (!this.animation && temp) {
-          this.animation = temp;
-        }
-
-        if (window.scrollY === 0 && this.animation) {
-          this.animation = false;
-        }
-
-      }
-
-  }
-
 }
