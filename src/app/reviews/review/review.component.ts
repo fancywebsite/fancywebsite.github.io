18af0511612e728ae2e5135f1fb5d9a5e788ff6a
TimeaNagyGarwan <timea.nagy@garwan.sk>
2023-05-23 15:34:39 +0200

    vimiova review, show more

--- src/app/reviews/review/review.component.ts
+++ src/app/reviews/review/review.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-review',
@@ -6,4 +7,17 @@ import { Component, Input } from '@angular/core';
 })
 export class ReviewComponent {
     @Input() id = '';
+
+    showMore = false;
+
+    isThereMore = false;
+
+    constructor(protected translate: TranslateService){}
+
+    getText(){
+      const text = String(this.translate.instant('reviews.' + this.id + '.text'));
+      const index = text.indexOf('$');
+      this.isThereMore = index != -1;
+      return this.showMore || !this.isThereMore ? text.replace('$', '') : text.substring(0, index);
+    }
 }
